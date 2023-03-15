e893ac9750ab93743e177e2e6b288a1130aa167e
cupokyle <kyleforsythcomedy@gmail.com>
2023-03-15 16:28:11 -0400

    more changes

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -5,199 +5,231 @@ import RudeDude from './RudeDude'; // Import RudeDude component
 import { createDeck, calculatePoints } from '../helpers/deck';
 
 const GameBoard = () => {
-    const [deck, setDeck] = useState([]);
-    const [playerHand, setPlayerHand] = useState([]);
-    const [dealerHand, setDealerHand] = useState([]);
-    const [gameStatus, setGameStatus] = useState(''); // '', 'win', 'lose', 'draw'
-    const [rudeMessage, setRudeMessage] = useState('');
-    const [showBlackjackAnimation, setShowBlackjackAnimation] = useState(false);
+  const [deck, setDeck] = useState([]);
+  const [playerHand, setPlayerHand] = useState([]);
+  const [dealerHand, setDealerHand] = useState([]);
+  const [gameStatus, setGameStatus] = useState(''); // '', 'win', 'lose', 'draw'
+  const [rudeMessage, setRudeMessage] = useState('');
+  const [showBlackjackAnimation, setShowBlackjackAnimation] = useState(false);
 
 
-    useEffect(() => {
-        startNewGame();
-    }, []);
+  useEffect(() => {
+    startNewGame();
+  }, []);
 
-    useEffect(() => {
-        if (playerHand.length === 2 && calculatePoints(playerHand) === 21) {
-          setTimeout(() => {
-            setShowBlackjackAnimation(true);
-            setTimeout(() => {
-              setGameStatus('win');
-            }, 2000); // Wait for the animation to complete before setting the game status to 'win'
-          }, 100); // Brief delay for animation
-        }
-      }, [playerHand]);
-    
-
-    function startNewGame() {
-        const newDeck = createDeck();
-        const newPlayerHand = [newDeck.pop(), newDeck.pop()];
-        const newDealerHand = [newDeck.pop(), { ...newDeck.pop(), hidden: true }];
-
-        setDeck(newDeck);
-        setPlayerHand(newPlayerHand);
-        setDealerHand(newDealerHand);
-        setGameStatus('');
-    }
-
-    function displayHandTotal(hand) {
-        const total = calculatePoints(hand);
-        return `Total: ${total}`;
-    }
-
-    function getRandomRudeMessage(action) {
-        const rudeMessages = {
-            hit: [
-                "Going for another card, huh?",
-                "Oh, you need more help!",
-                "Hit me? More like, hit yourself!",
-            ],
-            stand: [
-                "Giving up already?",
-                "Scared to hit, I see!",
-                "That's it? I expected more.",
-            ],
-            jokes: [
-                "Why did the player go broke? Because they bet their life on a game of blackjack!",
-                "Why are you so bad at blackjack? Even a broken clock is right twice a day!",
-                "What's the difference between you and a deck of cards? A deck of cards has a few aces!",
-                "Why did the player keep losing? They thought the game was called 'BleakJack'!",
-            ],
-        };
-
-        const actionMessages = rudeMessages[action];
-        const jokeMessages = rudeMessages.jokes;
-        const allMessages = [...actionMessages, ...jokeMessages];
-
-        return allMessages[Math.floor(Math.random() * allMessages.length)];
-    }
-
-
-    function handleHit() {
-        if (gameStatus) return;
-        const newDeck = [...deck];
-        const newPlayerHand = [...playerHand, newDeck.pop()];
-        setDeck(newDeck);
-        setPlayerHand(newPlayerHand);
-      
-        const maxCardIndex = Math.max(newPlayerHand.length, dealerHand.length);
-        const animationDelay = (maxCardIndex + 1) * 250; // Convert delay to milliseconds
-      
+  useEffect(() => {
+    if (playerHand.length === 2 && calculatePoints(playerHand) === 21) {
+      setTimeout(() => {
+        setShowBlackjackAnimation(true);
         setTimeout(() => {
-          const playerPoints = calculatePoints(newPlayerHand);
-          if (playerPoints > 21) {
-            setGameStatus('lose');
-          } else if (playerPoints === 21) {
-            setShowBlackjackAnimation(true);
-            setTimeout(() => {
-              setGameStatus('win');
-            }, 2000); // Wait for the animation to complete before setting the game status to 'win'
-          }
-        }, animationDelay);
-      
-        if (Math.random() < 0.5) {
-          setRudeMessage(getRandomRudeMessage('hit'));
-      
+          setGameStatus('win');
+        }, 2000); // Wait for the animation to complete before setting the game status to 'win'
+      }, 100); // Brief delay for animation
+    }
+  }, [playerHand]);
+
+
+  function startNewGame() {
+    const newDeck = createDeck();
+    const newPlayerHand = [newDeck.pop(), newDeck.pop()];
+    const newDealerHand = [newDeck.pop(), { ...newDeck.pop(), hidden: true }];
+
+    setDeck(newDeck);
+    setPlayerHand(newPlayerHand);
+    setDealerHand(newDealerHand);
+    setGameStatus('');
+  }
+
+  function displayHandTotal(hand) {
+    const total = calculatePoints(hand);
+    return `Total: ${total}`;
+  }
+
+  function getRandomRudeMessage(action) {
+    const rudeMessages = {
+      hit: [
+        "Going for another card, huh?",
+        "Oh, you need more help!",
+        "Hit me? More like, hit yourself!",
+        "Keep hitting, you'll just make it worse!",
+        "You know hitting isn't always the answer, right?",
+        "Desperate for a better hand? Good luck!",
+      ],
+      stand: [
+        "Giving up already?",
+        "Scared to hit, I see!",
+        "That's it? I expected more.",
+        "You're really banking on the dealer to bust, huh?",
+        "Do you think standing will save you?",
+        "Afraid of going over 21? I don't blame you.",
+      ],
+      jokes: [
+        "Why did the player go broke?\n They kept betting their rent money on blackjack!",
+        "I heard your love life is like your blackjack skills\n – always busting!",
+        "Why are you so bad at blackjack?\n Even your dog could make better decisions!",
+        "What's the difference between you and a deck of cards?\n A deck of cards doesn't live in their mom's basement!",
+        "Why did the player keep losing?\n They thought the game was called 'BleakJack'!",
+        "You must be great at your job\n – I assume it doesn't involve decision making!",
+        "Why did the blackjack player bring a pencil and paper to the casino?\n They thought they could draw a better life!",
+        "What do you and a blackjack table have in common?\n You both get dealt with!",
+        "Why did the blackjack player go to jail?\n They couldn't stay out of the hole!",
+        "What do you call a blackjack player who always loses?\n A regular!",
+        "Why did the blackjack player always wear sunglasses?\n To hide the tears from their lonely nights!",
+        "Did you know that playing blackjack and dating have something in common?\n You're terrible at both!",
+      ],
+      win: [
+        "Ugh, you won this time, but I still don't like you.",
+        "Congrats, you got lucky. Don't get used to it.",
+        "Wow, you actually won. Even a blind squirrel finds a nut sometimes.",
+        "You won, but you're still not cool.",
+        "Fine, you won. But don't think it makes you special.",
+      ],
+    };
+
+    const actionMessages = rudeMessages[action];
+    const jokeMessages = rudeMessages.jokes;
+    const allMessages = [...actionMessages, ...jokeMessages];
+
+    return allMessages[Math.floor(Math.random() * allMessages.length)];
+  }
+
+  function handleHit() {
+    if (gameStatus) return;
+    const newDeck = [...deck];
+    const newPlayerHand = [...playerHand, newDeck.pop()];
+    setDeck(newDeck);
+    setPlayerHand(newPlayerHand);
+
+    const maxCardIndex = Math.max(newPlayerHand.length, dealerHand.length);
+    const animationDelay = (maxCardIndex + 1) * 250; // Convert delay to milliseconds
+
+    setTimeout(() => {
+      const playerPoints = calculatePoints(newPlayerHand);
+      if (playerPoints > 21) {
+        setGameStatus('lose');
+      } else if (playerPoints === 21) {
+        setShowBlackjackAnimation(true);
+        setTimeout(() => {
+          setGameStatus('win');
+          // Display a win message from Rude Dude
+          setRudeMessage(getRandomRudeMessage('win'));
           setTimeout(() => {
             setRudeMessage('');
-          }, 3000);
-        }
+          }, 4000);
+        }, 2000); // Wait for the animation to complete before setting the game status to 'win'
       }
-      
-      
+    }, animationDelay);
 
-      function handleStand() {
-        if (gameStatus) return;
-        let newDealerHand = [...dealerHand];
-        let newDeck = [...deck];
-        newDealerHand[1].hidden = false;
-      
-        while (calculatePoints(newDealerHand) < 17) {
-          newDealerHand.push(newDeck.pop());
-        }
-      
-        const playerPoints = calculatePoints(playerHand);
-        const dealerPoints = calculatePoints(newDealerHand);
-      
-        setDealerHand(newDealerHand);
-        setDeck(newDeck);
-      
-        // Calculate the maximum animation delay for cards
-        const maxCardIndex = Math.max(playerHand.length, dealerHand.length);
-        const animationDelay = (maxCardIndex + 1) * 250; // Convert delay to milliseconds
-      
-        // Show Blackjack animation if the player has a Blackjack at the start
-        if (playerPoints === 21) {
-          setTimeout(() => {
-            setShowBlackjackAnimation(true);
-          }, 2000); // Brief delay for animation
-        }
-      
+    // Only display rude messages when the game is still ongoing
+    if (Math.random() < 0.39 && !gameStatus) {
+      setRudeMessage(getRandomRudeMessage('hit'));
+
+      setTimeout(() => {
+        setRudeMessage('');
+      }, 4000);
+    }
+  }
+
+
+
+
+  function handleStand() {
+    if (gameStatus) return;
+    let newDealerHand = [...dealerHand];
+    let newDeck = [...deck];
+    newDealerHand[1].hidden = false;
+
+    while (calculatePoints(newDealerHand) < 17) {
+      newDealerHand.push(newDeck.pop());
+    }
+
+    const playerPoints = calculatePoints(playerHand);
+    const dealerPoints = calculatePoints(newDealerHand);
+
+    setDealerHand(newDealerHand);
+    setDeck(newDeck);
+
+    // Calculate the maximum animation delay for cards
+    const maxCardIndex = Math.max(playerHand.length, dealerHand.length);
+    const animationDelay = (maxCardIndex + 1) * 250; // Convert delay to milliseconds
+
+    // Show Blackjack animation if the player has a Blackjack at the start
+    if (playerPoints === 21) {
+      setTimeout(() => {
+        setShowBlackjackAnimation(true);
+      }, 2000); // Brief delay for animation
+    }
+
+    setTimeout(() => {
+      let newGameStatus = '';
+      if (dealerPoints > 21 || playerPoints > dealerPoints) {
+        newGameStatus = 'win';
+        // Display a win message from Rude Dude
+        setRudeMessage(getRandomRudeMessage('win'));
         setTimeout(() => {
-          let newGameStatus = '';
-          if (dealerPoints > 21 || playerPoints > dealerPoints) {
-            newGameStatus = 'win';
-          } else if (dealerPoints === playerPoints) {
-            newGameStatus = 'draw';
-          } else {
-            newGameStatus = 'lose';
-          }
-      
-          setGameStatus(newGameStatus);
-        }, animationDelay);
-        
-        if (Math.random() < 0.5) {
-          setRudeMessage(getRandomRudeMessage('stand'));
-      
-          setTimeout(() => {
-            setRudeMessage('');
-          }, 3000);
-        }
+          setRudeMessage('');
+        }, 4000);
+      } else if (dealerPoints === playerPoints) {
+        newGameStatus = 'draw';
+      } else {
+        newGameStatus = 'lose';
       }
-      
+
+      setGameStatus(newGameStatus);
+    }, animationDelay);
+
+    if (Math.random() < 0.39 && !gameStatus) {
+      setRudeMessage(getRandomRudeMessage('stand'));
+
+      setTimeout(() => {
+        setRudeMessage('');
+      }, 4000);
+    }
+  }
 
 
 
-    return (
-        <div className="game-board">
-            <div className="dealer-section">
-                <div className="dealer-label">
-                    Dealer's Hand {gameStatus && `(${displayHandTotal(dealerHand)})`}
-                </div>
-                <div className="dealer-hand">
-                    {dealerHand.map((card, index) => (
-                        <Card key={index} card={card} delay={index} />
-                    ))}
-                </div>
-            </div>
-            <div className="player-section">
-                <div className="player-label">
-                    Player's Hand ({displayHandTotal(playerHand)})
-                </div>
-                <div className="player-hand">
-                    {playerHand.map((card, index) => (
-                        <Card key={index} card={card} delay={index} />
-                    ))}
-                </div>
-            </div>
-            <div className="buttons">
-                <button onClick={startNewGame}>New Game</button>
-                <button onClick={handleHit}>Hit</button>
-                <button onClick={handleStand}>Stand</button>
-            </div>
-            {rudeMessage && <RudeDude message={rudeMessage} />}
-            {gameStatus && (
-                <div className={`game-result ${gameStatus}`}>
-                    {gameStatus === 'win' && 'You Win!'}
-                    {gameStatus === 'lose' && 'You Lose!'}
-                    {gameStatus === 'draw' && 'It\'s a Draw!'}
-                </div>
-            )}
-            {showBlackjackAnimation && (
-                <BlackjackAnimation onAnimationEnd={() => setShowBlackjackAnimation(false)} />
-            )}
+
+  return (
+    <div className="game-board">
+      <div className="dealer-section">
+        <div className="dealer-label">
+          Dealer's Hand {gameStatus && `(${displayHandTotal(dealerHand)})`}
         </div>
-    );
+        <div className="dealer-hand">
+          {dealerHand.map((card, index) => (
+            <Card key={index} card={card} delay={index} />
+          ))}
+        </div>
+      </div>
+      <div className="player-section">
+        <div className="player-label">
+          Player's Hand ({displayHandTotal(playerHand)})
+        </div>
+        <div className="player-hand">
+          {playerHand.map((card, index) => (
+            <Card key={index} card={card} delay={index} />
+          ))}
+        </div>
+      </div>
+      <div className="buttons">
+        {gameStatus !== '' && <button onClick={startNewGame}>New Game</button>}
+        <button onClick={handleHit}>Hit</button>
+        <button onClick={handleStand}>Stand</button>
+      </div>
+      {rudeMessage && <RudeDude message={rudeMessage} />}
+      {gameStatus && (
+        <div className={`game-result ${gameStatus}`}>
+          {gameStatus === 'win' && 'You Win!'}
+          {gameStatus === 'lose' && 'You Lose!'}
+          {gameStatus === 'draw' && 'It\'s a Draw!'}
+        </div>
+      )}
+      {showBlackjackAnimation && (
+        <BlackjackAnimation onAnimationEnd={() => setShowBlackjackAnimation(false)} />
+      )}
+    </div>
+  );
 };
 
 export default GameBoard;
